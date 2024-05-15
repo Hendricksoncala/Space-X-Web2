cdaafaaaaadeb6a2e3441604c99b5ccb4ea2b4e0
Hendricksoncala <156349949+Hendricksoncala@users.noreply.github.com>
2024-05-14 21:39:53 -0500

    feat: :sparkles: Retomando las partes que se habian creado pero no subido

--- js/modulesComponents/inform.js
+++ js/modulesComponents/inform.js
@@ -1,37 +1,37 @@
 import {
-    getAllRocketsEngineTotal,
-    getAllRocketsEngineThrustVacuumTotal,
+    getAllRocketEngineTotal,
+    getAllRocketEngineThrustVacuumTotal
 } from "../modules/rockets.js";
 
+
 export const informRocketEngineThrustSeaLevel = async(thrust_sea_level)=>{
-    let {kN:totalKN} = await getAllRocketsEngineTotal();
-    let pocentaje = (thrust_sea_level + 100) / totalKN;
+    let {kN:totalKN} = await getAllRocketEngineTotal();
+    let pocentaje = (thrust_sea_level.kN * 100) / totalKN
 
-    let div = document.createElement("div");   
+    let div = document.createElement('div');
     div.classList.add("carousel__item")
-    let divlist = document.createElement("div");
-    divlist.classList.add("item_progress_bar");
-    divFirst.style = `background: radial-gradient(closest-side, #1d1f38 85%, transparent 85% 100%), conic-gradient(var(--color--three) ${pocentaje}%, transparent 0)`
-    let divFirstChildren = document.createElement("div");
-    divFirstChildren.classList.add("progress_value");
-    let strong = document.createElement("strong");
+    let divFirst = document.createElement('div');
+    divFirst.classList.add("item__progress__bar");
+    divFirst.style = `background: radial-gradient(closest-side, #1d1f38 85%, transparent 85% 100%), conic-gradient(var(--color-quinto) ${pocentaje}%, transparent 0)`
+    let divFirstChildren = document.createElement('div');
+    divFirstChildren.classList.add("progress__value")
+    let strong = document.createElement('strong');
     strong.textContent = "Atmospheric acceleration"
-    let smallFirst = document.createElement("small")
+    let smallFirst = document.createElement('small');
     smallFirst.textContent = `${pocentaje.toFixed(2)} %`
-
-    let smallLast = document.createElement("small");
+    
+    let smallLast = document.createElement('small');
     let kN = new Intl.NumberFormat('cop').format(thrust_sea_level.kN)
     let lbf = new Intl.NumberFormat('cop').format(thrust_sea_level.lbf)
-    smallLast.innerHTML = `${kN} kN <br> / ${lbf} lbf`
+    smallLast.innerHTML = `${kN} kN <br> ${lbf} Lbf`
 
     divFirstChildren.append(strong, smallFirst, smallLast)
-    divlist.append(divFirstChildren)
+    divFirst.append(divFirstChildren)
     div.append(divFirst)
     let section__information__1 = document.querySelector("#section__information__1");
     section__information__1.innerHTML = "";
     section__information__1.append(div)
 
-    //the result of all the processes are this:
     // <div class="carousel__item">
     //     <div class="item__progress__bar">
     //         <div class="progress__value">
@@ -42,31 +42,40 @@ export const informRocketEngineThrustSeaLevel = async(thrust_sea_level)=>{
     // </div>
 }
 
-export const infoRocketEngineThrustVacuum = async(thrust_vacuum)=>{
-    let {kN:totalKN} = await getAllRocketsEngineThrustVacuumTotal();
+
+export const informRocketEngineThrustVacuum = async(thrust_vacuum)=>{
+    let {kN:totalKN} = await getAllRocketEngineThrustVacuumTotal();
     let pocentaje = (thrust_vacuum.kN * 100) / totalKN;
 
-    let div = document.createElement("div");   
+    let div = document.createElement('div');
     div.classList.add("carousel__item")
-    let divlist = document.createElement("div");
-    divlist.classList.add("item__progress__bar");
-    divFirst.style = `background: radial-gradient(closest-side, #1d1f38 85%, transparent 85% 100%), conic-gradient(var(--color--three) ${pocentaje}%, transparent 0)`
-    let divFirstChildren = document.createElement("div");
-    divFirstChildren.classList.add("progress__value");
-    let strong = document.createElement("strong");
+    let divFirst = document.createElement('div');
+    divFirst.classList.add("item__progress__bar");
+    divFirst.style = `background: radial-gradient(closest-side, #1d1f38 85%, transparent 85% 100%), conic-gradient(var(--color-quinto) ${pocentaje}%, transparent 0)`
+    let divFirstChildren = document.createElement('div');
+    divFirstChildren.classList.add("progress__value")
+    let strong = document.createElement('strong');
     strong.textContent = "Speed in space"
-    let smallFirst = document.createElement("small")
+    let smallFirst = document.createElement('small');
     smallFirst.textContent = `${pocentaje.toFixed(2)} %`
-
-    let smallLast = document.createElement("small");
+    
+    let smallLast = document.createElement('small');
     let kN = new Intl.NumberFormat('cop').format(thrust_vacuum.kN)
     let lbf = new Intl.NumberFormat('cop').format(thrust_vacuum.lbf)
-    smallLast.innerHTML = `${kN} kN <br> / ${lbf} lbf`
+    smallLast.innerHTML = `${kN} kN <br> ${lbf} Lbf`
 
     divFirstChildren.append(strong, smallFirst, smallLast)
-    divlist.append(divFirstChildren)
+    divFirst.append(divFirstChildren)
     div.append(divFirst)
     let section__information__1 = document.querySelector("#section__information__1");
-    section__information__1.innerHTML = "";
     section__information__1.append(div)
+
+    // <div class="carousel__item">
+    //     <div class="item__progress__bar">
+    //         <div class="progress__value">
+    //             <strong>Title</strong>
+    //             <small>3</small>
+    //         </div>
+    //     </div>
+    // </div>
 }
--- js/modulesComponents/information.js
+++ js/modulesComponents/information.js
@@ -1,20 +1,23 @@
 export const informationRockets = async(country, description)=>{
     let div = document.createElement('div');
-    div.classList.add('description__container')
+    div.classList.add('leftbox')
     let divFirst = document.createElement('div');
-    let img = document.createElement('img');
-    img.setAttribute("src", "storage/img/icons/mech.svg")
-    divFirst.append(img);
-
+    divFirst.classList.add("icon")
+    let icon = document.createElement('i');
+    icon.setAttribute("class", "bx bxs-check-circle");
+    icon.style.color = "#9499c3";
+    divFirst.append(icon);
+    
     let divLast = document.createElement('div');
-    let h3 = document.createElement('h3');
-    h3.textContent = country;
-    let small = document.createElement('small');
-    small.textContent = description;
-    divLast.append(h3, small);
+    divLast.classList.add("leftboxcontent")
+    let h4 = document.createElement('h4');
+    h4.textContent = country
+    let p = document.createElement('p');
+    p.textContent = description
+    divLast.append(h4, p);
     div.append(divFirst, divLast);
 
-    let description__item = document.querySelector('#descriptio__item')
+    let description__item = document.querySelector("#description__item")
     description__item.append(div)
 
     // <div class="description__container">
@@ -23,58 +26,31 @@ export const informationRockets = async(country, description)=>{
     //     </div>
     //     <div>
     //         <h3>Title</h3>
-    //         <small>Lorem ipsum dolor sit amet...</small>
+    //         <p>Lorem ipsum dolor sit amet...</p>
     //     </div>
     // </div>
 }
-
-export const  informationLaunchCostRocket = async(cost_per_launch) => {
-    let div = document.createElement('div')
-    div.classList.add('description__container');
-    let divFirst = document.createElement('div');
-    let img = document.createElement('img');
-    img.setAttribute("src", "storage/img/icons/mech.svg")
-
-    let divLast = document.wrirte('div');
-    let h3 = document.createElement('h3');
-    h3.textContent = "The estimated cost per rocket launch"
-    let small = document.createElement('small')
-    let money = new Intl.NumberFormat('cop').format(cost_per_launch)
-    small.textContent = `$ ${money}`
-    divLast.append(h3, small);
-
-    let description__item = document.querySelector('#description__item')
-    description__item.append(div)
-
-    // <div class="description__container">
-    //     <div>
-    //         <img src="http://www.example.com">
-    //     </div>
-    //     <div>
-    //         <h3>Title</h3>
-    //         <small>Lorem ipsum dolor sit amet...</small>
-    //     </div>
-    // </div>
-}
-
-
-export const informatonFirstFlightRocket = async(first_flight)=>{
+export const informationLaunchCostRocket = async(cost_per_launch)=>{
     let div = document.createElement('div');
-    div.classList.add('description__container')
+    div.classList.add('leftbox')
     let divFirst = document.createElement('div');
-    let img = document.createElement('img')
-    img.setAttribute("src", "storage/img/icons/mech.svg")
-    divFirst.append(img);
-
+    divFirst.classList.add("icon")
+    let icon = document.createElement('i');
+    icon.setAttribute("class", "bx bxs-check-circle");
+    icon.style.color = "#9499c3";
+    divFirst.append(icon);
+    
     let divLast = document.createElement('div');
-    let h3 = document.createElement('h3');
-    h3.textContent = "the date of the first flight of the rocket"
-    let small = document.createElement('small');
-    small.textContent = first_flight;
-    divLast.append(h3, small);
-    div.append(divFirst,divLast);
+    divLast.classList.add("leftboxcontent")
+    let h4 = document.createElement('h4');
+    h4.textContent = "The estimated cost per rocket launch"
+    let p = document.createElement('p');
+    let money = new Intl.NumberFormat('cop').format(cost_per_launch)
+    p.textContent = `$ ${money}`
+    divLast.append(h4, p);
+    div.append(divFirst, divLast);
 
-    let description__item = document.querySelector('#descriptio__item')
+    let description__item = document.querySelector("#description__item")
     description__item.append(div)
 
     // <div class="description__container">
@@ -83,29 +59,72 @@ export const informatonFirstFlightRocket = async(first_flight)=>{
     //     </div>
     //     <div>
     //         <h3>Title</h3>
-    //         <small>Lorem ipsum dolor sit amet...</small>
+    //         <p>Lorem ipsum dolor sit amet...</p>
     //     </div>
     // </div>
 }
+export const informationFirstFlightRocket = async(first_flight)=>{
+    let div = document.createElement('div');
+    div.classList.add('leftbox')
+    let divFirst = document.createElement('div');
+    divFirst.classList.add("icon")
+    let icon = document.createElement('i');
+    icon.setAttribute("class", "bx bxs-check-circle");
+    icon.style.color = "#9499c3";
+    divFirst.append(icon);
+    
+    let divLast = document.createElement('div');
+    divLast.classList.add("leftboxcontent")
+    let h4 = document.createElement('h4');
+    h4.textContent = "The date of the first flight of the rocket"
+    let p = document.createElement('p');
+    p.textContent = first_flight
+    divLast.append(h4, p);
+    div.append(divFirst, divLast);
 
-export const informationWebRocket = async(wikipedia) => {
-    let div =document.createElement('div')
-    div.classList.add('descroiptoion_container');
-    let divFirst = document.createElement('div')
-    let img = document.createElement('img')
-    img.setAttribute("src",  "storage/img/icons/mech.svg")
-    divFirst.apppend(img)
+    let description__item = document.querySelector("#description__item")
+    description__item.append(div)
 
-    let divLast = document.createElement('div')
-    let h3 = document.createElement('h3')
-    h3.textContent = "Read more about the coete"
+    // <div class="description__container">
+    //     <div>
+    //         <img src="http://www.example.com">
+    //     </div>
+    //     <div>
+    //         <h3>Title</h3>
+    //         <p>Lorem ipsum dolor sit amet...</p>
+    //     </div>
+    // </div>
+}
+export const informationWebRocket = async(wikipedia)=>{
+    let div = document.createElement('div');
+    div.classList.add('leftbox')
+    let divFirst = document.createElement('div');
+    divFirst.classList.add("icon")
+    let icon = document.createElement('i');
+    icon.setAttribute("class", "bx bxs-check-circle");
+    icon.style.color = "#9499c3";
+    divFirst.append(icon);
+    
+    let divLast = document.createElement('div');
+    divLast.classList.add("leftboxcontent")
+    let h4 = document.createElement('h4');
+    h4.textContent = "Read more about the coete"
     let a = document.createElement('a');
     a.setAttribute("href", wikipedia)
     a.setAttribute("target", "_blank")
-    a.textContent = "wikipedia"
-    divLast.append(h3, a)
+    a.textContent = "Wikipedia"
+    divLast.append(h4, a);
     div.append(divFirst, divLast);
-    let description__item = document.querySelector("#description_item")
+    let description__item = document.querySelector("#description__item")
     description__item.append(div)
 
+    // <div class="description__container">
+    //     <div>
+    //         <img src="http://www.example.com">
+    //     </div>
+    //     <div>
+    //         <h3>Title</h3>
+    //         <a href="#" target="_blank">Lorem</a>
+    //     </div>
+    // </div>
 }
